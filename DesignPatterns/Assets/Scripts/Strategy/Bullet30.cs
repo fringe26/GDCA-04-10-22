9e273524daaf4da28063039488f67f8cd9bf72fe
taliyevcode@gmail.com <taliyevcode@gmail.com>
2022-12-29 16:13:39 +0400

    Ziya

---
+++ DesignPatterns/Assets/Scripts/Strategy/Bullet30.cs
@@ -0,0 +1,10 @@
+﻿public class Bullet30 : Bullet_base
+{
+    private new int _damage;
+    public int Damage => _damage;
+    
+    public override void Initialize()
+    {
+        _damage = 30;
+    }
+}
---
+++ DesignPatterns/Assets/Scripts/Strategy/Bullet50.cs
@@ -0,0 +1,10 @@
+﻿public class Bullet50 : Bullet_base
+{
+    private new int _damage;
+    public int Damage => _damage;
+    
+    public override void Initialize()
+    {
+        _damage = 50;
+    }
+}
